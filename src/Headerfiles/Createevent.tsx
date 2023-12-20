cbc9c47f7119fd8f02b8e0ebe8b5d8598c489fda
Alisha Mendonca <mealisha@trellissoft.ai>
2023-12-20 11:50:39 +0530

    event calendar
    undefined

--- src/Headerfiles/Createevent.tsx
+++ src/Headerfiles/Createevent.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Formik } from "formik";
-
+import * as Yup from 'yup';
 import UsersService from "../Axios/UsersService";
 
 interface EventFormData {
@@ -10,11 +10,42 @@ interface EventFormData {
   date: string;
   time: string;
   location: string;
-  participantList: string;
+  participantList: string[];
   roles: string;
   mode: string | undefined;
   meetlink: string;
+  eventDocument:File | null;
 }
+const validateForm = (values: { location?: string | undefined; meetlink?: string | undefined; eventname: string; date: string; time: string; participantList: string[]; roles: string; mode: NonNullable<"online" | "offline" | undefined>; }) => {
+  //const errors: Partial<Record<keyof EventFormData, string>> = {};
+
+  if ((values.mode === 'online' && !values.meetlink) || (values.mode === 'offline' && !values.location)) {
+    throw new Yup.ValidationError('Validation failed', values, 'conditionalValidation');
+  }
+  // if (!Array.isArray(values.participantList) || values.participantList.length === 0) {
+  //   throw new Yup.ValidationError('Please select at least one participant', values, 'participantListValidation');
+  // }
+
+};
+
+const validationSchema = Yup.object({
+  eventname: Yup.string().max(50).required("This field is required"),
+  date: Yup.string().required("This field is required"),
+  time: Yup.string().required("This field is required"),
+  location: Yup.string(),
+  participantList: Yup.array().min(1, "Please select at least one participant").required("This field is required"),
+  roles: Yup.string().max(100).required("This field is required"),
+  mode: Yup.string().oneOf(["online", "offline"]).required("This field is required"),
+  meetlink: Yup.string(),
+  eventDocument:Yup.mixed().test('fileSize','File size is too large.maximum size allowed is 10MB',(value)=>{
+    if(!value){
+      return true;
+    }
+    return (value as File).size <= 1024 * 1024 * 10;
+  })
+}).test('conditionalValidation', 'Validation failed', validateForm);
+
+
 
 const Createevent: React.FC = () => {
   const [eventlist, setEventList] = useState({
@@ -22,43 +53,66 @@ const Createevent: React.FC = () => {
     date: "",
     time: "",
     location: "",
-    participantList: "",
+    participantList: [] as string[],
     roles: "",
     mode: undefined,
     meetlink: "",
+    eventDocument: null as File|null,
   });
+  const [userArray, setUserArray] = useState<Array<{ fullname: string, id: number }>>([]);
 
   const usersService = UsersService();
+  useEffect(() => {
+    // Fetch user data when the component mounts
+    const fetchUsers = async () => {
+      try {
+        const response = await usersService().getUserList(); // Implement getUsers method in your Axios service
+        setUserArray(response.data); // Assuming the structure is { "users": [...] }
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   const addevent = async (values: EventFormData) => {
-    console.log("Form Data:", values);
-    try {
-      await usersService().addEvent(values);
-      setEventList({
-        eventname: "",
-        date: "",
-        time: "",
-        location: "",
-        participantList: "",
-        roles: "",
-        mode: undefined,
-        meetlink: "",
-      });
-      console.log("Event added successfully");
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  };
 
+   try {
+    await usersService().addEvent(values);
+    setEventList({
+      eventname: "",
+      date: "",
+      time: "",
+      location: "",
+      participantList: [],
+      roles: "",
+      mode: undefined,
+      meetlink: "",
+      eventDocument: null as File|null,
+    });
+    console.log("Event added successfully");
+  } catch (error) {
+
+    console.error("Error submitting form:", error);
+  }
+    
+    
+   
+  };
+  //const userArray=usersService().getUserList();
   return (
     <Container>
       <Row className="justify-content-center mt-5">
         <Col xs={12} md={8}>
           <Formik
             initialValues={eventlist}
-            onSubmit={(values) => {
+            validationSchema={validationSchema}
+            onSubmit={(values:EventFormData) => {
               console.log("Form submitted with values:", values);
-              addevent(values);
+            
+                addevent(values);
+            
             }}
           >
             {({
@@ -66,7 +120,11 @@ const Createevent: React.FC = () => {
               handleChange,
               handleBlur,
               handleSubmit,
+              setFieldValue,
               isSubmitting,
+              errors,
+              touched,
+
             }) => (
               <Form onSubmit={handleSubmit}>
                 <Form.Group>
@@ -77,7 +135,12 @@ const Createevent: React.FC = () => {
                     onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.eventname}
+                    className={errors.eventname && touched.eventname ? "is-invalid" : ""}
                   />
+                  {errors.eventname && touched.eventname && (
+                    <p className="error">{errors.eventname}</p>
+                  )}
+
                 </Form.Group>
 
                 <Form.Group>
@@ -88,7 +151,12 @@ const Createevent: React.FC = () => {
                     onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.date}
+                    className={errors.date && touched.date ? "is-invalid" : ""}
                   />
+                  {errors.date && touched.date && (
+                    <p className="error">{errors.date}</p>
+                  )}
+
                 </Form.Group>
 
                 <Form.Group>
@@ -99,21 +167,75 @@ const Createevent: React.FC = () => {
                     onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.time}
+                    className={errors.time && touched.time ? "is-invalid" : ""}
                   />
-                </Form.Group>
+                 {errors.time && touched.time && (
+                    <p className="error">{errors.time}</p>
+                  )}
 
-                <Form.Group>
-                  <Form.Label>Location:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="location"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.location}
-                  />
                 </Form.Group>
-
                 <Form.Group>
+  <Form.Label>Participant List:</Form.Label>
+  {userArray.map((user) => (
+    <div key={user.id}>
+      <Form.Check
+        type="checkbox"
+        id={`participant-${user.id}`}
+        label={user.fullname}
+        name="participantList"
+        value={user.fullname}
+        checked={values.participantList.includes(user.fullname)}
+        onChange={(e) => {
+          const isChecked = e.target.checked;
+          setFieldValue("participantList", isChecked
+            ? [...values.participantList, user.fullname]
+            : values.participantList.filter((name) => name !== user.fullname));
+        }}
+      />
+    </div>
+  ))}
+  {errors.participantList && touched.participantList && (
+    <p className="error">{errors.participantList}</p>
+  )}
+</Form.Group>
+
+              
+
+                {/* <Form.Group>
+                  <Form.Label>Participant List:</Form.Label>
+                  {userArray.map((user) => (
+                    <div key={user.id}>
+                      <Form.Check
+                        type="checkbox"
+                        id={`participant-${user.id}`}
+                        label={user.fullname}
+                        name="participantList"
+                        value={user.fullname}
+                        checked={values.participantList.includes(user.fullname)}
+                        onChange={(e) => {
+                          const isChecked = e.target.checked;
+                          setEventList((prev) => ({
+                            ...prev,
+                            participantList: isChecked
+                              ? [...prev.participantList, user.fullname]
+                              : prev.participantList.filter((name) => name !== user.fullname),
+                          }));
+                        }}
+                      />
+                    </div>
+                  ))}
+                  {errors.participantList && touched.participantList && (
+                    <p className="error">{errors.participantList}</p>
+                  )}
+                </Form.Group> */}
+
+
+                
+
+
+                
+
+                {/* <Form.Group>
                   <Form.Label>Participant List:</Form.Label>
                   <Form.Control
                     type="text"
@@ -121,8 +243,13 @@ const Createevent: React.FC = () => {
                     onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.participantList}
+                    className={errors.participantList && touched.participantList ? "is-invalid" : ""}
                   />
-                </Form.Group>
+                  {errors.participantList && touched.participantList && (
+                    <p className="error">{errors.participantList}</p>
+                  )}
+
+                </Form.Group> */}
 
                 <Form.Group>
                   <Form.Label>Roles:</Form.Label>
@@ -132,33 +259,94 @@ const Createevent: React.FC = () => {
                     onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.roles}
+                    className={errors.roles && touched.roles ? "is-invalid" : ""}
                   />
-                </Form.Group>
+                  {errors.roles && touched.roles && (
+                    <p className="error">{errors.roles}</p>
+                  )}
 
+                </Form.Group>
                 <Form.Group>
                   <Form.Label>Mode:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="mode"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.mode}
-                  />
+                  <div>
+                    <Form.Check
+                      inline
+                      label="Online"
+                      type="radio"
+                      name="mode"
+                      value="online"
+                      checked={values.mode === "online"}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <Form.Check
+                      inline
+                      label="Offline"
+                      type="radio"
+                      name="mode"
+                      value="offline"
+                      checked={values.mode === "offline"}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </div>
+                  {errors.mode && touched.mode && <p className="error">{errors.mode}</p>}
                 </Form.Group>
 
+
+
+                {values.mode==="online" && (
+                      <Form.Group>
+                      <Form.Label>Meeting Link:</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="meetlink"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.meetlink}
+                        className={errors.meetlink && touched.meetlink ? "is-invalid" : ""}
+                      />
+                       {errors.meetlink && touched.meetlink && (
+                      <p className="error">{errors.meetlink}</p>
+                    )}
+                    </Form.Group>
+                )}
+                {values.mode==="offline" && (
+                    <Form.Group>
+                    <Form.Label>Location:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="location"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.location}
+                      className={errors.location && touched.location ? "is-invalid" : ""}
+  
+                    />
+                    {errors.location && touched.location && (
+                      <p className="error">{errors.location}</p>
+                    )}
+  
+                  </Form.Group>
+                )}
                 <Form.Group>
-                  <Form.Label>Meeting Link:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="meetlink"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.meetlink}
-                  />
-                </Form.Group>
+                    <Form.Label>Upload Document:</Form.Label>
+                    <Form.Control
+                      type="file"
+                      name="eventDocument"
+                      onChange={(e) => setFieldValue("eventDocument", (e.currentTarget as HTMLInputElement).files?.[0] || null)}
+                      onBlur={handleBlur}
+                      className={errors.eventDocument && touched.eventDocument ? "is-invalid" : ""}
+                    />
+                    {errors.eventDocument && touched.eventDocument && (
+                      <p className="error">{errors.eventDocument}</p>
+                    )}
+                  </Form.Group>
+
+
 
                 <Button variant="primary" type="submit" disabled={isSubmitting}>
-                  Submit
+                  Create Event
                 </Button>
               </Form>
             )}
@@ -167,6 +355,7 @@ const Createevent: React.FC = () => {
       </Row>
     </Container>
   );
+ 
 };
 
 export default Createevent;
---
+++ src/Headerfiles/MyCalendar.tsx
@@ -0,0 +1,47 @@
+import { useState,useEffect } from "react";
+import UsersService from "../Axios/UsersService";
+import moment from "moment";
+import {Calendar,momentLocalizer} from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer= momentLocalizer(moment);
+
+
+
+const MyCalendar = () => {
+
+  const usersService = UsersService();
+  const [events,setEvents]=useState<any[]>([]);
+
+  useEffect(()=>{
+    const fetchEvents=async ()=>{
+      try{
+        const response=await usersService().getEventList();
+        setEvents(response.data);
+      }catch(error){
+        console.error('Error fetching events:',error);
+      }
+
+    };
+    fetchEvents();
+  },[]);
+
+  const eventList=events.map((event)=>({
+    title:event.eventname,
+    start:new Date(`${event.date} ${event.time}`),
+    end: new Date(`${event.date} ${event.time}`),
+  }));
+
+  return (
+    <>
+      <Calendar 
+        localizer={localizer}
+        events={eventList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{height:600,backgroundColor: '#F2F1EB'}}
+       />
+    </>
+  )
+}
+export default MyCalendar;
