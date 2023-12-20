b7baa3ce3c232114307296b6e1e91aed6a2e25ee
Alisha Mendonca <mealisha@trellissoft.ai>
2023-12-20 17:49:32 +0530

    updated edit event

---
+++ src/Headerfiles/EditEvent.tsx
@@ -0,0 +1,314 @@
+import React, { useState, useEffect } from "react";
+import { Button, Card,Form,Modal } from "react-bootstrap";
+import UsersService from "../Axios/UsersService";
+
+interface Event{
+  id:number;
+  eventname:string;
+  date: string;
+  time: string;
+  location: string;
+  participantList: string[] | string;
+  roles: string;
+  mode: string;
+  meetlink: string;
+  eventDocument: string;
+
+}
+const EditEvent: React.FC = () => {
+  const usersService = UsersService(); 
+  const [eventList, setEventList] = useState<Event[]>([]);
+  const [updateEventDetails,setUpdateEventDetails]=useState<Event>({
+    id:0,
+    eventname:'',
+    date:'',
+    time:'',
+    location: '',
+    participantList:'',
+    roles:'',
+    mode:'',
+    meetlink:'',
+    eventDocument: '',
+  });
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  //const [eventIdToUpdate, setEventIdToUpdate] = useState<number | null>(null);
+  const [userList, setUserList] = useState<string[]>([]);
+
+useEffect(() => {
+    // Fetch the list of usernames from the users array
+    const fetchUserList = async () => {
+      try {
+        const response = await usersService().getUserList();
+        debugger
+        const users = response.data;
+        const usernames = users.map((user: any) => user.fullname);
+        setUserList(usernames);
+      } catch (error) {
+        console.error("Error fetching user list:", error);
+      }
+    };
+
+    fetchUserList();
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      // Create an instance of the service
+      const response = await usersService().getEventList(); // Call the getEventList method
+
+      setEventList(response.data || []); // Assuming the structure is { "events": [...] }
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  useEffect(() => {
+   
+    fetchEvents();
+  }, []);
+  const deleteEvent= async(id:number)=>{
+    try{
+        const response = await usersService().deleteEvent(id);
+        console.log("response",response);
+        console.log("Event deleted successfully");
+        fetchEvents();
+    } catch (error: any){
+        console.error("Error in deleting the user",error);
+    }
+};
+
+const updateEvent= async (data:any)=>{
+  try{
+    // const response=await usersService().updateEvent(id,updateEventDetails);
+    // console.log("response",response.data);
+    // const eventDetails = await usersService().getEventList();
+    //setUpdateEventDetails(eventDetails.data);
+    // console.log(eventDetails);
+
+    // setUpdateEventDetails({  ...eventDetails.data, id: eventDetails.data.id  });
+    // console.log("Event updated successfully");
+    // fetchEvents();
+    console.log('event', data)
+    setUpdateEventDetails({
+      id:data.id,
+      eventname:data.eventname,
+      date: data.date,
+      time: data.time,
+      location: data.location,
+      participantList: data.participantList,
+      //participantList: data.participantList,
+      roles: data.roles,
+      mode: data.mode,
+      meetlink: data.meetlink,
+      eventDocument: data.eventDocument
+    }
+    )
+    setShowUpdateModal(true);
+    
+
+
+  } catch(error:any){
+    console.log('Error',error);
+
+  }
+ 
+};
+const handleUpdateSubmit = async () => {
+  try {
+    await usersService().updateEvent(updateEventDetails.id, updateEventDetails);
+    setUpdateEventDetails({
+      id:0,
+      eventname:'',
+      date:'',
+      time:'',
+      location: '',
+      participantList:'',
+      roles:'',
+      mode:'',
+      meetlink:'',
+      eventDocument: '',
+
+    });
+    setShowUpdateModal(false);
+    fetchEvents();
+  } catch (error) {
+    console.error("Error updating event:", error);
+  }
+};
+
+const handleCloseUpdateModal = () => {
+  setUpdateEventDetails({
+    id:0,
+    eventname:'',
+    date:'',
+    time:'',
+    location: '',
+    participantList:'',
+    roles:'',
+    mode:'',
+    meetlink:'',
+    eventDocument: '',
+  });
+  setShowUpdateModal(false);
+};
+
+
+  // const handleDelete = (eventId:number) => {
+    
+  //   console.log(`Delete event with ID ${eventId}`);
+  // };
+
+  // const handleUpdate = (eventId:number) => {
+   
+  //   console.log(`Update event with ID ${eventId}`);
+  // };
+
+  return (
+    <div>
+      <h2>Edit Events</h2>
+      {eventList.map((event) => (
+        <Card key={event.id} className="mb-3">
+          <Card.Body>
+            <Card.Title>{event.eventname}</Card.Title>
+           
+            <Button variant="danger" onClick={() => deleteEvent(event.id)}>
+              Delete
+            </Button>
+            <Button variant="info" onClick={() => updateEvent(event)}>
+              Update
+            </Button>
+          </Card.Body>
+        </Card>
+      ))}
+          <Modal show={showUpdateModal} onHide={handleCloseUpdateModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Event</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {updateEventDetails && (
+            <Form>
+              <Form.Group controlId="formEventName">
+                <Form.Label>Event Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter event name"
+                  value={updateEventDetails.eventname}
+                  onChange={(e) =>
+                    setUpdateEventDetails((prev) => ({ ...prev, eventname: e.target.value }) as Event)
+                  }
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formDate">
+                <Form.Label>Date</Form.Label>
+                <Form.Control
+                  type="date"
+                  value={updateEventDetails.date}
+                  onChange={(e) =>
+                    setUpdateEventDetails((prev) => ({ ...prev, date: e.target.value }) as Event)
+                  }
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formTime">
+                <Form.Label>Time</Form.Label>
+                <Form.Control
+                  type="time"
+                  value={updateEventDetails.time}
+                  onChange={(e) =>
+                    setUpdateEventDetails((prev) => ({ ...prev, time: e.target.value }) as Event)
+                  }
+                />
+              </Form.Group>
+              <Form.Group controlId="formLocation">
+  <Form.Label>Location</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="Enter location"
+    value={updateEventDetails.location}
+    onChange={(e) =>
+      setUpdateEventDetails((prev) => ({ ...prev, location: e.target.value }) as Event)
+    }
+  />
+</Form.Group>
+<Form.Group controlId="formParticipantList">
+    <Form.Label>Participant List</Form.Label>
+    {userList.map((username) => (
+      <Form.Check
+        key={username}
+        type="checkbox"
+        label={username}
+        checked={updateEventDetails?.participantList.includes(username)}
+        onChange={(e) => {
+          const isChecked = e.target.checked;
+          setUpdateEventDetails((prev) => ({
+            ...prev,
+            participantList: isChecked
+              ? [...prev.participantList, username]
+              : prev.participantList.filter((name) => name !== username),
+          }) as Event);
+        }}
+      />
+    ))}
+  </Form.Group>
+
+
+<Form.Group controlId="formRoles">
+  <Form.Label>Roles</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="Enter roles"
+    value={updateEventDetails.roles}
+    onChange={(e) =>
+      setUpdateEventDetails((prev) => ({ ...prev, roles: e.target.value })as Event)
+    }
+  />
+</Form.Group>
+
+<Form.Group controlId="formMode">
+  <Form.Label>Mode</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="Enter mode"
+    value={updateEventDetails.mode}
+    onChange={(e) =>
+      setUpdateEventDetails((prev) => ({ ...prev, mode: e.target.value })as Event)
+    }
+  />
+</Form.Group>
+
+<Form.Group controlId="formMeetlink">
+  <Form.Label>Meet Link</Form.Label>
+  <Form.Control
+    type="text"
+    placeholder="Enter meet link"
+    value={updateEventDetails.meetlink}
+    onChange={(e) =>
+      setUpdateEventDetails((prev) => ({ ...prev, meetlink: e.target.value })as Event)
+    }
+  />
+</Form.Group>
+
+
+
+
+
+            </Form>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseUpdateModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleUpdateSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
+    </div>
+  );
+};
+
+export default EditEvent;
